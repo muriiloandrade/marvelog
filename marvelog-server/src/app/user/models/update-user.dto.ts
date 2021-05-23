1d20540225eb1804de1a304102400ee1941a259c
Murilo A <muriloandradealmeida@gmail.com>
2021-05-23 03:44:51 -0300

    feat(server): :label: add update user dto

---
+++ marvelog-server/src/app/user/models/update-user.dto.ts
@@ -0,0 +1,8 @@
+import { OmitType } from '@nestjs/mapped-types';
+import { RegisterDTO } from '@app/auth/models/register.dto';
+
+export class UpdateUserDTO extends OmitType(RegisterDTO, [
+  'password',
+  'username',
+  'email',
+]) {}
