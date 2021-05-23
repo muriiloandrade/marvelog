af4682b457be25e34bf8bccb21d8c2000ea4ec41
Murilo A <muriloandradealmeida@gmail.com>
2021-05-23 15:01:10 -0300

    feat(server): :sparkles: add route that returns user favorite comics

--- marvelog-server/src/app/comic/controller/comic.controller.ts
+++ marvelog-server/src/app/comic/controller/comic.controller.ts
@@ -1,16 +1,27 @@
+import { ComicService } from '@app/comic/service/comic.service';
 import {
   Controller,
+  Get,
   UseFilters,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { User } from '@shared/decorators/usuario.decorator';
 import { GeneralErrorsFilter } from '@shared/filters/error-handling.filter';
 import { HttpExceptionFilter } from '@shared/filters/http-exception.filter';
 import { JwtAuthGuard } from '@shared/guards/jwt-auth.guard';
+import { JwtDTO } from '@shared/interfaces/jwt.dto';
 
 @UseGuards(JwtAuthGuard)
 @UseFilters(GeneralErrorsFilter, HttpExceptionFilter)
 @UsePipes(new ValidationPipe({ transform: true, forbidUnknownValues: true }))
-@Controller('comic')
-export class ComicController {}
+@Controller('v1/comic')
+export class ComicController {
+  constructor(private service: ComicService) {}
+
+  @Get('favorites')
+  async getFavoritesByUser(@User() user: JwtDTO) {
+    return this.service.getFavorites(user.sub);
+  }
+}
--- marvelog-server/src/app/comic/service/comic.service.ts
+++ marvelog-server/src/app/comic/service/comic.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { ModelClass } from 'objection';
+import { ComicModel } from '@app/comic/models/comic.model';
+import { FavoriteComicModel } from '@shared/models/favoriteComic.model';
 
 @Injectable()
-export class ComicService {}
+export class ComicService {
+  constructor(
+    @Inject('ComicModel') private modelClass: ModelClass<ComicModel>,
+    @Inject('FavoriteComicModel')
+    private favoriteModelClass: ModelClass<FavoriteComicModel>,
+  ) {}
+
+  async getFavorites(cod_user_usr: string) {
+    return this.favoriteModelClass
+      .query()
+      .withGraphFetched('comic')
+      .where({ cod_user_usr });
+  }
+}
