d646636589620501164ac043953ff219b3b3a8c3
Murilo A <muriloandradealmeida@gmail.com>
2021-05-23 00:36:19 -0300

    feat(server): :sparkles: add route get user by id

--- marvelog-server/src/app/user/controller/user.controller.ts
+++ marvelog-server/src/app/user/controller/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
+import { UserService } from '@app/user/service/user.service';
+import { JwtAuthGuard } from '@shared/guards/jwt-auth.guard';
 
-@Controller('user')
-export class UserController {}
+@Controller('v1/user')
+export class UserController {
+  constructor(private service: UserService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getUserById(@Param('id') cod_user_usr: string) {
+    const user = await this.service.getById(cod_user_usr);
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado!');
+    }
+    delete user.str_password_usr;
+    return user;
+  }
+}
