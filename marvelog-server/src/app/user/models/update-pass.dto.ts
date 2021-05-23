013ca1a1fc11d6a02b13e86177c3879753605035
Murilo A <muriloandradealmeida@gmail.com>
2021-05-23 03:59:47 -0300

    feat(server): :label: add update pass dto

---
+++ marvelog-server/src/app/user/models/update-pass.dto.ts
@@ -0,0 +1,13 @@
+import { IsNotEmpty, MinLength } from 'class-validator';
+import { PickType } from '@nestjs/mapped-types';
+import { RegisterDTO } from '@app/auth/models/register.dto';
+
+export class UpdatePassDTO extends PickType(RegisterDTO, ['password']) {
+  @IsNotEmpty({
+    message: 'O campo senha antiga é obrigatório!',
+  })
+  @MinLength(8, {
+    message: 'A senha antiga deve conter no mínimo 8 caracteres!',
+  })
+  oldpassword: string;
+}
