03f829ab1cb462fe81d0921787d89b9fbfc7fd02
Murilo A <muriloandradealmeida@gmail.com>
2021-05-24 12:53:11 -0300

    refactor(server): :poop: fix return from search characters to fewer properties
    The return from search characters was too big, because of a lot of other unnecessary (to the system) properties that Marvel returns from their route. With this implementation I removed some of those properties.

--- marvelog-server/src/app/marvel/models/charactersResp.dto.ts
+++ marvelog-server/src/app/marvel/models/charactersResp.dto.ts
@@ -1,34 +1,77 @@
-export interface Characters {
-  code: number;
-  data: Data;
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable max-classes-per-file */
+export class ComicsItem {
+  resourceURI: string;
+  name: string;
+
+  constructor(partial: ComicsItem) {
+    this.resourceURI = partial.resourceURI;
+    this.name = partial.name;
+  }
 }
 
-export interface Data {
-  results: Result[];
+export class Thumbnail {
+  path: string;
+  extension: string;
+
+  constructor(partial: Thumbnail) {
+    this.path = partial.extension;
+    this.extension = partial.extension;
+  }
+}
+export class Comics {
+  available: string;
+  returned: string;
+  collectionURI: string;
+  items: ComicsItem[];
+
+  constructor(partial: Comics) {
+    const { available, items, returned, collectionURI, ...trash } = partial;
+
+    this.available = available;
+    this.returned = returned;
+    this.collectionURI = collectionURI;
+    this.items = items;
+  }
 }
 
-export interface Result {
+export class Result {
   id: string;
   name: string;
   description: string;
   modified: string;
   thumbnail: Thumbnail;
   comics: Comics;
+
+  constructor(partial: Result) {
+    const { comics, description, id, name, thumbnail, modified, ...trash } =
+      partial;
+
+    this.comics = comics;
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.modified = modified;
+    this.thumbnail = thumbnail;
+    this.comics = comics;
+  }
 }
 
-export interface Comics {
-  available: string;
-  returned: string;
-  collectionURI: string;
-  items: ComicsItem[];
+export class Data {
+  results: Result[];
+
+  constructor(partial: Data) {
+    const { results, ...trash } = partial;
+    this.results = results;
+  }
 }
 
-export interface ComicsItem {
-  resourceURI: string;
-  name: string;
-}
+export class Characters {
+  code: number;
+  data: Data;
 
-export interface Thumbnail {
-  path: string;
-  extension: string;
+  constructor(partial: Characters) {
+    const { data, ...trash } = partial;
+    this.data = data;
+  }
 }
--- marvelog-server/src/app/marvel/services/marvel.service.ts
+++ marvelog-server/src/app/marvel/services/marvel.service.ts
@@ -8,28 +8,28 @@ import { Md5 } from 'ts-md5';
 import { catchError, map } from 'rxjs/operators';
 import { EnvService } from '@config/env/env.service';
 import { MarvelParamsDTO } from '@app/marvel/models/marvel.dto';
-import { Characters } from '@app/marvel/models/charactersResp.dto';
+import { Characters, Result } from '@app/marvel/models/charactersResp.dto';
 
 @Injectable()
 export class MarvelService {
   constructor(private http: HttpService, private env: EnvService) {}
 
-  searchCharacters(params: MarvelParamsDTO) {
+  searchCharacters(searchParams: MarvelParamsDTO) {
     return this.http
       .get<Characters>(`${this.env.marvelApiUrl}/characters`, {
         params: {
           ...this.makeRequestParams(),
-          ...params,
+          ...searchParams,
         },
       })
       .pipe(
-        map((response) => {
-          return {
-            code: response.data.code,
-            data: {
-              results: response.data.data.results,
-            },
-          } as Characters;
+        map((marvelResponse) => {
+          const results: Result[] = [];
+          const { data } = marvelResponse.data;
+          data.results.forEach((searchResult) => {
+            results.push(new Result(searchResult));
+          });
+          return results;
         }),
         catchError(() => {
           throw new PreconditionFailedException(
