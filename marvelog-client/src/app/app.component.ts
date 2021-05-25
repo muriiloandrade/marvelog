d502cb1df8abf36577ae9e672435bba3380e5047
Murilo A <muriloandradealmeida@gmail.com>
2021-05-25 01:11:13 -0300

    refactor(client): :fire: remove unused code

--- marvelog-client/src/app/app.component.ts
+++ marvelog-client/src/app/app.component.ts
@@ -3,8 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'marvelog-client';
-}
+export class AppComponent {}
