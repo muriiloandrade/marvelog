7ccdef5e6c44e73f0457cd32b642252081fa7e2a
Murilo A <muriloandradealmeida@gmail.com>
2021-05-25 01:19:28 -0300

    feat(client): add locale config and httpclientmodule to appmodule

--- marvelog-client/src/app/app.module.ts
+++ marvelog-client/src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
@@ -10,9 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
