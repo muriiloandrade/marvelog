376c64330605519e9592565f95a9941eab775c6d
Murilo A <muriloandradealmeida@gmail.com>
2021-05-29 18:59:24 -0300

    fix(server): :card_file_box: make last modified fields nullable
    Some characters and comics from Marvel's API doesn't have the field "modified" fulfilled and this was causing a bug where you couldn't like this particular comics and characters

---
+++ marvelog-server/src/config/knex/migrations/20210529184902_AddNullabilityToCharacterLastModified.ts
@@ -0,0 +1,21 @@
+import * as Knex from 'knex';
+
+const tableName = 'character';
+
+export async function up(knex: Knex) {
+  return knex.schema.alterTable(tableName, (t) => {
+    t.dateTime('dat_lastmodified_cha')
+      .nullable()
+      .comment("Date that the character was last modified in Marvel's API")
+      .alter();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.alterTable(tableName, (t) => {
+    t.dateTime('dat_lastmodified_cha')
+      .notNullable()
+      .comment("Date that the character was last modified in Marvel's API")
+      .alter();
+  });
+}
---
+++ marvelog-server/src/config/knex/migrations/20210529185532_AddNullabilityToComicLastModified.ts
@@ -0,0 +1,21 @@
+import * as Knex from 'knex';
+
+const tableName = 'comic';
+
+export async function up(knex: Knex) {
+  return knex.schema.alterTable(tableName, (t) => {
+    t.dateTime('dat_lastmodified_com')
+      .nullable()
+      .comment("Date that the comic was last modified in Marvel's API")
+      .alter();
+  });
+}
+
+export async function down(knex: Knex) {
+  return knex.schema.alterTable(tableName, (t) => {
+    t.dateTime('dat_lastmodified_com')
+      .notNullable()
+      .comment("Date that the comic was last modified in Marvel's API")
+      .alter();
+  });
+}
